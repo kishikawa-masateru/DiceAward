d0509d9c3e5b59009bc2e0eacb06273dc8dc73f2
masateru kishikawa <kishimasa0614@gmail.com>
2020-09-13 03:45:20 +0900

    Main ファイルの中身を実装

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,87 @@
 package app;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import operator.AwardList;
+
 public class Main {
+
+	static AwardList awardList;		// 報酬リスト
+	static Scanner sc;
+
+	static {
+		awardList = new AwardList(System.getProperty("user.dir") + "\\DiceAward\\award\\award.txt");
+		sc = new Scanner(System.in);
+	}
 
 	public static void main(String[] args) {
 
+		// プログラム終了時に awardList の報酬をファイルに上書きする
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> awardList.writeToFile()));
+
+		// コマンドライン引数がない場合は，ランダムな報酬を出力する
+		if (args.length == 0) {
+
+			int index = new Random().nextInt(awardList.count());	// 報酬の数の範囲内で乱数を取得
+
+			// 乱数が示す報酬を出力
+			System.out.println(awardList.get(index));
+		}
+
+		// コマンドライン引数がある場合は，その種類に応じて処理を分岐する
+		else {
+			switch (args[0]) {
+			case "set":
+				// 編集前状態を表示
+				awardList.show();
+
+				// 指定した報酬を上書きする
+				awardList.replase(getIndex(), getAward());
+				break;
+			case "add":
+				// 編集前状態を表示
+				awardList.show();
+
+				// 報酬を追加する
+				awardList.add(getAward());
+				break;
+			case "remove":
+				// 編集前状態を表示
+				awardList.show();
+
+				// 指定した報酬を削除する
+				awardList.remove(getIndex());
+				break;
+			case "show":
+				// 中身の表示
+				awardList.show();
+				break;
+			}
+
+			// 結果の表示
+			System.out.println();
+			awardList.show();
+			sc.close();
+		}
+	}
+
+	/**
+	 * コンソールから，報酬を取得する
+	 * @return 報酬
+	 */
+	private static String getAward() {
+		System.out.print("報酬...");
+		return sc.next();
+	}
+
+	/**
+	 * コンソールから，インデックスを取得する
+	 * @return 編集する報酬リストのインデックス
+	 */
+	private static int getIndex() {
+		System.out.print("インデックス...");
+		return sc.nextInt();
 	}
 
 }
