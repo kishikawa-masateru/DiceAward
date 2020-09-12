4f4a18532f6df0dd70f13bd73d061222d72f4d3f
masateru kishikawa <kishimasa0614@gmail.com>
2020-09-12 20:30:43 +0900

    AwardListの骨格を作成

--- src/operator/AwardList.java
+++ src/operator/AwardList.java
@@ -13,4 +13,61 @@ import java.util.List;
 public class AwardList {
 	private List<String> awardList;	// 報酬リスト
 	private String awardPath;		// 報酬リストを書き込むクラス
+
+	/**
+	 * コンストラクタ<br>
+	 *
+	 * 1．引数で指定したパスで awardPath を初期化<br>
+	 * 2．awardPath が指しているファイルから報酬を読み込む<br>
+	 * 3．読み込んだ報酬を awardList に追加<br>
+	 */
+	public AwardList(String awardPath) {
+
+	}
+
+	/**
+	 * index 番目の報酬を award に置き換える
+	 */
+	public void replase(int index, String award) {
+
+	}
+
+	/**
+	 * @param index
+	 * @return index 番目の報酬
+	 */
+	public String get(int index) {
+
+	}
+
+	/**
+	 * @return 報酬の数
+	 * @return
+	 */
+	public int count() {
+
+	}
+
+	/**
+	 * 報酬リストの末尾に報酬を追加する
+	 * @param award
+	 */
+	public void add(String award) {
+
+	}
+
+	/**
+	 * 報酬のリストをファイルに書き込む
+	 */
+	public void writeToFile() {
+
+	}
+
+	/**
+	 * index 番目の報酬を削除する
+	 * @param index 報酬リストで削除したいインデックス
+	 */
+	public void remove(int index) {
+
+	}
 }
