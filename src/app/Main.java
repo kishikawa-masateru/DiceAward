83a876475c7bf66060189e84c93f97056f5381d7
Masateru.Kishikawa <kishimasa0614@gmail.com>
2020-09-14 10:50:19 +0900

    OSに応じてパスの区切り文字を設定できるようにした
    エラーの原因だった，報酬リストファイルのパスを正しいパスを指すように変更

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.nio.file.Path;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,11 +8,26 @@ import operator.AwardList;
 
 public class Main {
 
-	static AwardList awardList;		// 報酬リスト
+	static AwardList awardList; // 報酬リスト
+	static String PATH_SEPARATOR; // パスの区切り文字
 	static Scanner sc;
 
 	static {
-		awardList = new AwardList(System.getProperty("user.dir") + "\\DiceAward\\award\\award.txt");
+		// OSに応じてパスの区切り文字を設定する
+		String os_name = System.getProperty("os.name").toLowerCase();
+		if (os_name.contains("windows")) {
+			PATH_SEPARATOR = "\\";
+		} else if (os_name.contains("mac") || os_name.contains("linux")) {
+			PATH_SEPARATOR = "/";
+		}
+
+		// 報酬リストファイルのパスを作成
+		String user_dir_path = System.getProperty("user.dir");
+		String project_path = user_dir_path.substring(0, user_dir_path.lastIndexOf(PATH_SEPARATOR));
+		String award_path = project_path + PATH_SEPARATOR + "src" + PATH_SEPARATOR + "award" + PATH_SEPARATOR
+				+ "award.txt";
+
+		awardList = new AwardList(award_path);
 		sc = new Scanner(System.in);
 	}
 
@@ -23,7 +39,7 @@ public class Main {
 		// コマンドライン引数がない場合は，ランダムな報酬を出力する
 		if (args.length == 0) {
 
-			int index = new Random().nextInt(awardList.count());	// 報酬の数の範囲内で乱数を取得
+			int index = new Random().nextInt(awardList.count()); // 報酬の数の範囲内で乱数を取得
 
 			// 乱数が示す報酬を出力
 			System.out.println(awardList.get(index));
@@ -32,31 +48,34 @@ public class Main {
 		// コマンドライン引数がある場合は，その種類に応じて処理を分岐する
 		else {
 			switch (args[0]) {
-			case "set":
-				// 編集前状態を表示
-				awardList.show();
+				case "set":
+					// 編集前状態を表示
+					awardList.show();
 
-				// 指定した報酬を上書きする
-				awardList.replase(getIndex(), getAward());
-				break;
-			case "add":
-				// 編集前状態を表示
-				awardList.show();
+					// 指定した報酬を上書きする
+					awardList.replase(getIndex(), getAward());
+					break;
+				case "add":
+					// 編集前状態を表示
+					awardList.show();
 
-				// 報酬を追加する
-				awardList.add(getAward());
-				break;
-			case "remove":
-				// 編集前状態を表示
-				awardList.show();
+					// 報酬を追加する
+					awardList.add(getAward());
+					break;
+				case "remove":
+					// 編集前状態を表示
+					awardList.show();
 
-				// 指定した報酬を削除する
-				awardList.remove(getIndex());
-				break;
-			case "show":
-				// 中身の表示
-				awardList.show();
-				break;
+					// 指定した報酬を削除する
+					awardList.remove(getIndex());
+					break;
+				case "show":
+					// 中身の表示
+					awardList.show();
+
+					// そのまま終了
+					sc.close();
+					System.exit(0);
 			}
 
 			// 結果の表示
@@ -68,6 +87,7 @@ public class Main {
 
 	/**
 	 * コンソールから，報酬を取得する
+	 * 
 	 * @return 報酬
 	 */
 	private static String getAward() {
@@ -77,6 +97,7 @@ public class Main {
 
 	/**
 	 * コンソールから，インデックスを取得する
+	 * 
 	 * @return 編集する報酬リストのインデックス
 	 */
 	private static int getIndex() {
